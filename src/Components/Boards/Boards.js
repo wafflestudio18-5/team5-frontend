f831df6cf0f46e88888a368a5b61e9ad9bc69fa5
우현민 <woohyunmin@uhyeonmin-ui-MacBookPro.local>
2020-11-24 18:04:32 +0900

    틀 다 잡았음

--- src/Components/Boards/Boards.js
+++ src/Components/Boards/Boards.js
@@ -1,32 +1,29 @@
 import "./Boards.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrello } from "@fortawesome/free-brands-svg-icons";
+import { faUser, faHome, faUserFriends, faCog, faBriefcase } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
 
 const temp_board1 = {
-  title: "Waffle-18.5-toyproject-team5"
-}
+  title: "Waffle-18.5-toyproject-team5",
+};
 
 const temp_board2 = {
-  title: "temporary board"
-}
+  title: "temporary board",
+};
 
-const temp_recent = [
-  temp_board1,
-  temp_board2
-]
+const temp_recent = [temp_board1, temp_board2];
 
-const temp_personal = [
-  temp_board1,
-]
+const temp_personal = [temp_board1];
 
-const temp_workspace = [
-  temp_board2
-]
+const temp_workspace = [temp_board2];
 
 const title_to_board = (title, i) => (
   <li className="board-wrapper" key={i}>
     {title}
   </li>
-)
+);
 
 function Boards() {
   return (
@@ -34,33 +31,40 @@ function Boards() {
       <section className="left-buttons">
         <ul>
           <li className="left-button-wrapper" id="Boards-wrapper">
-            <img className="left-button-img" alt="o" />
-            <p className="left-button-desc">Boards</p>
+            <FontAwesomeIcon icon={faTrello} />
+            <p className="left-button-desc" id="boards">
+              Boards
+            </p>
           </li>
           <li className="left-button-wrapper" id="Templates-wrapper">
-            <img className="left-button-img" alt="o" />
-            <p className="left-button-desc">Templates</p>
+            <FontAwesomeIcon icon={faTrello} />
+            <p className="left-button-desc" id="templates">
+              Templates
+            </p>
           </li>
           <li className="left-button-wrapper" id="Home-wrapper">
-            <img className="left-button-img" alt="o" />
-            <p className="left-button-desc">Home</p>
+            <FontAwesomeIcon icon={faHome} />
+            <p className="left-button-desc" id="home">
+              Home
+            </p>
           </li>
         </ul>
-        <div className="Teams">
+        <div className="Teams-hdr">
           <p>Teams</p>
           <p>+</p>
         </div>
-        <ul>
+        <ul className="Teams">
           <li>
-            Trello Workspace
+            <FontAwesomeIcon icon={faUserFriends} />
+            <b> Trello Workspace</b>
           </li>
         </ul>
       </section>
       <section className="main-boards">
         <div className="boards-desc" id="recently">
           <div>
-          <img alt="o"/>
-          <h4>Recently Viewed</h4>
+            <FontAwesomeIcon icon={faClock} />
+            <h4>Recently Viewed</h4>
           </div>
         </div>
         <ul className="boards-boards" id="recently">
@@ -69,34 +73,39 @@ function Boards() {
 
         <div className="boards-desc" id="personal">
           <div>
-          <img alt="o"/>
-          <h4>Personal Boards</h4>
+            <FontAwesomeIcon icon={faUser} />
+            <h4>Personal Boards</h4>
           </div>
         </div>
         <ul className="boards-boards" id="personal">
           {temp_personal.map((item, i) => title_to_board(item.title, i))}
-          <li className="board-wrapper create">
-            Create new board
-          </li>
+          <li className="board-wrapper create">Create new board</li>
         </ul>
 
         <div className="boards-desc" id="workspace">
           <div>
-            <img alt="o"/>
+            <FontAwesomeIcon icon={faUserFriends} />
             <h4>Trello workspace</h4>
           </div>
           <nav>
-            <Link to={"https://trello.com/userworkspace" + "(유저 고유번호인듯)"}>Boards</Link>
-            <Link to={"https://trello.com/userworkspace" + "(유저 고유번호인듯)" + "/members"}>Members</Link>
-            <Link to={"https://trello.com/userworkspace" + "(유저 고유번호인듯)" + "/account"}>Settings</Link>
-            <p>Upgrade</p>
+            <Link to={`https://trello.com/userworkspace${"유저 고유번호"}`}>
+              <FontAwesomeIcon className="tw_icon" icon={faTrello} />
+              Boards
+            </Link>
+            <Link to={`https://trello.com/userworkspace${"유저 고유번호"}/members`}>
+              <FontAwesomeIcon className="tw_icon" icon={faUser} />
+              Members
+            </Link>
+            <Link to={`https://trello.com/userworkspace${"유저 고유번호"}/account`}>
+              <FontAwesomeIcon className="tw_icon" icon={faCog} />
+              Settings
+            </Link>
+            <p><FontAwesomeIcon className="tw_icon" icon={faBriefcase} />Upgrade</p>
           </nav>
         </div>
         <ul className="boards-boards" id="workspace">
           {temp_workspace.map((item, i) => title_to_board(item.title, i))}
-          <li className="board-wrapper create">
-            Create new board
-          </li>
+          <li className="board-wrapper create">Create new board</li>
         </ul>
       </section>
     </div>
---
+++ src/Components/Header/assets.js
@@ -0,0 +1,112 @@
+export const svg_hl_1 = (
+  <svg
+    width="24"
+    height="24"
+    role="presentation"
+    focusable="false"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M4 5a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zm0 6a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1v-2zm7-7a1 1 0 00-1 1v2a1 1 0 001 1h2a1 1 0 001-1V5a1 1 0 00-1-1h-2zm-1 7a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 01-1 1h-2a1 1 0 01-1-1v-2zm7-7a1 1 0 00-1 1v2a1 1 0 001 1h2a1 1 0 001-1V5a1 1 0 00-1-1h-2zm-1 7a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 01-1 1h-2a1 1 0 01-1-1v-2zM5 16a1 1 0 00-1 1v2a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 00-1-1H5zm5 1a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 01-1 1h-2a1 1 0 01-1-1v-2zm7-1a1 1 0 00-1 1v2a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 00-1-1h-2z"
+      fill="white"
+    />
+  </svg>
+);
+
+export const svg_hl_2 = (
+  <svg
+    width="24"
+    height="24"
+    role="presentation"
+    focusable="false"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3.586 10.414A2 2 0 003 11.828V19a2 2 0 002 2h5a1 1 0 001-1v-6h2v6a1 1 0 001 1h5a2 2 0 002-2v-7.172a2 2 0 00-.586-1.414l-7.707-7.707a1 1 0 00-1.414 0l-7.707 7.707zM13 12a2 2 0 012 2v5h4v-7.172l-7-7-7 7V19h4v-5a2 2 0 012-2h2z"
+      fill="white"
+    />
+  </svg>
+);
+
+export const svg_hl_3 = (
+  <svg
+    width="24"
+    height="24"
+    role="presentation"
+    focusable="false"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3 5a2 2 0 012-2h14a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5zm2 1a1 1 0 011-1h4a1 1 0 011 1v10a1 1 0 01-1 1H6a1 1 0 01-1-1V6zm9-1a1 1 0 00-1 1v6a1 1 0 001 1h4a1 1 0 001-1V6a1 1 0 00-1-1h-4z"
+      fill="white"
+    ></path>
+  </svg>
+);
+
+export const svg_hr_1 = (
+  <svg
+    width="24"
+    height="24"
+    role="presentation"
+    focusable="false"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M12 3a1 1 0 00-1 1v7H4a1 1 0 100 2h7v7a1 1 0 102 0v-7h7a1 1 0 100-2h-7V4a1 1 0 00-1-1z"
+      fill="white"
+    ></path>
+  </svg>
+);
+
+export const svg_hr_2 = (
+  <svg
+    width="24"
+    height="24"
+    role="presentation"
+    focusable="false"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 20a8 8 0 100-16 8 8 0 000 16zm0 2c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+      fill="white"
+    ></path>
+    <path
+      d="M11 11a1 1 0 112 0v5a1 1 0 11-2 0v-5zm2-3a1 1 0 11-2 0 1 1 0 012 0z"
+      fill="white"
+    ></path>
+  </svg>
+);
+
+export const svg_hr_3 = (
+  <svg
+    width="24"
+    height="24"
+    role="presentation"
+    focusable="false"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 2a1 1 0 00-1 1v.078a4.687 4.687 0 00-1.867.715 4.51 4.51 0 00-1.527 1.76 5.914 5.914 0 00-.53 1.658A5.908 5.908 0 007 7.972V8h1-1v3.723l-2.857 4.762A1 1 0 005 18h14a1 1 0 00.858-1.515L17 11.723V8h-1l1-.001v-.027a2.613 2.613 0 00-.01-.215 5.901 5.901 0 00-.066-.546 5.913 5.913 0 00-.53-1.658 4.51 4.51 0 00-1.527-1.76A4.687 4.687 0 0013 3.078V3a1 1 0 00-1-1zM9 7.988V12a1 1 0 01-.143.514L6.767 16h10.467l-2.092-3.486A1 1 0 0115 12V7.988l-.005-.089a3.903 3.903 0 00-.044-.36 3.92 3.92 0 00-.345-1.092 2.512 2.512 0 00-.848-.99C13.378 5.203 12.825 5 12 5s-1.377.203-1.758.457a2.512 2.512 0 00-.848.99A3.918 3.918 0 009.05 7.54a3.911 3.911 0 00-.05.45zM14 19a2 2 0 11-4 0h4z"
+      fill="white"
+    ></path>
+  </svg>
+);
+
+export const logo = (
+  <img
+    className="header_logo"
+    alt="logo"
+    id="trello_logo"
+    src="https://d2k1ftgv7pobq7.cloudfront.net/meta/u/res/images/trello-header-logos/af7af6ed478d3460709d715d9b3f74a4/trello-logo-white.svg"
+  />
+);
