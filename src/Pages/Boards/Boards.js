4ba15588ade7d76ac077ac32e685e70f2b3ec111
yuzzznk <69066745+yuzzznk@users.noreply.github.com>
2020-11-26 17:12:45 +0900

    Boards 왼쪽버튼 margin 수정

--- src/Pages/Boards/Boards.js
+++ src/Pages/Boards/Boards.js
@@ -1,5 +1,6 @@
 import Header from '../Header/Header';
 import { Boards } from '../../Components';
+import '../../Components/Boards/Boards.css'
 
 const get_user_data = (user_name) => {
   // TODO: 백에서 user name을 이용해 board list를 불러와야 한다.
@@ -15,10 +16,10 @@ function BoardsPage(props) {
   const user_data = get_user_data(user_name);
   
   return(
-    <>
+    <div id="pageBoardPage">
       <Header user_data={user_data} history={history}/>
       <Boards user_data={user_data} history={history}/>
-    </>
+    </div>
   )
 }
 
