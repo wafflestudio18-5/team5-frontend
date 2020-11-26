78eaa8800319cb515df83e85debf69a69ce96670
yuzzznk <69066745+yuzzznk@users.noreply.github.com>
2020-11-26 13:12:08 +0900

    카드 세로스크롤 O 리스트 가로스크롤 X

--- src/Components/Board/List.js
+++ src/Components/Board/List.js
@@ -4,9 +4,9 @@ import './Board.css';
 
 function List({listname, list}) {
     return (
-        <div className="board-list">
+        <div className="board-list"><div className="board-cards">
         <h3>{listname}</h3>
-            {list.map((card, index) => (<Card card={card} key={index} />))}
+            {list.map((card, index) => (<Card card={card} key={index} />))}</div>
         <button><span id="board-addcard-plus">+</span>Add another card</button>
         </div>
     )
