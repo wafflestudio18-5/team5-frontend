be8302f24ca3440675f8c24b97c96d7b2f39a0b1
yuzzznk <69066745+yuzzznk@users.noreply.github.com>
2020-11-26 10:35:52 +0900

    board 전용 헤더 만드는중. 아이콘 찾아봐야 할 듯

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -13,7 +13,12 @@ function Board({data}) {
           <option value="Calendar">Calendar</option>
           <option value="Map">Map</option>
           </select>
-          <h2>{data.board_name}</h2>
+          <h3>{data.board_name}</h3>
+          <button id="board-header-star">☆</button>
+          <button>{data.board_name}<div id="board-header-freeboard">free</div></button>
+          <button>Private</button>
+          <p>프사들동글동글</p>
+          <button>Invite</button>
         </div>
       
         <div id="board-header-right">
