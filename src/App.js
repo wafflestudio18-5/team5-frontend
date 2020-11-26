9ee02523a7ef0bf1cba9fdf7d69dfdc799eb14f8
yuzzznk <69066745+yuzzznk@users.noreply.github.com>
2020-11-26 16:00:47 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path={routes.Boards.path} component={routes.Boards.component} exact={true}/>
-        <Route path={routes.Login.path} component={routes.Login.component} exact={true}/>
-        <Route path={routes.SignUp.path} component={routes.SignUp.component} exact={true}/>
-        <Route path={routes.Home.path} component={routes.Home.component} exact={true}/>
-        <Route path={routes.Board.path} component={routes.Board.component} exact={true}/>
+        <Route path={routes.Boards.path} component={routes.Boards.component}/>
+        <Route path={routes.Board.path} component={routes.Board.component}/>
+        <Route path={routes.Login.path} component={routes.Login.component}/>
+        <Route path={routes.SignUp.path} component={routes.SignUp.component}/>
+        <Route path={routes.Home.path} component={routes.Home.component}/>
       </Switch>
     </BrowserRouter>
   );
