ff5ce7190a4d95309dd858bc9a73fb4511733800
yuzzznk <69066745+yuzzznk@users.noreply.github.com>
2020-12-24 00:36:28 +0900

    word-break 버그 수정 (한 리스트 안에 가로 스크롤바 안 생기게)

--- src/Components/Board/Card.js
+++ src/Components/Board/Card.js
@@ -3,19 +3,11 @@ import "./Board.css";
 
 function Card({ card, index }) {
   console.log(card);
-  if (index === 0) {
     return (
-      <div className="board-card" style={{ marginTop: 0 }}>
-        <p>{card.name}</p>
+      <div className="board-card" style={index === 0 ? { marginTop: 0 } : { marginTop: 10 }}>
+        <p style={{wordBreak: "break-all"}}>{card.name}</p>
       </div>
     );
-  } else {
-    return (
-      <div className="board-card" style={{ marginTop: 10 }}>
-        <p>{card.name}</p>
-      </div>
-    );
-  }
 }
 
 export default Card;
