84a574aa0a4cfa2ec9b4c873fdf8d71dc47a5ecd
yuzzznk <69066745+yuzzznk@users.noreply.github.com>
2020-12-25 18:31:54 +0900

    모달 바깥 클릭 시 모달이 닫히게 만들었습니다

--- src/Components/Board/Card.js
+++ src/Components/Board/Card.js
@@ -3,18 +3,24 @@ import { Link } from 'react-router-dom';
 import "./Board.css";
 import CardModal from '../CardModal/CardModal';
 
-function Card({ card, index }) {
+function Card({ card, index, board_key, board_name }) {
 
     const [cardPage, setCardPage] = useState(false);
 
     /*TODO key = card.key */
     const key = "def4u1tKey";
     const dashedName = card.name.replaceAll(" ", "-");
-    const cardPath = "/c/" + key + "/" + String(card.id) + "-" + dashedName; 
+    const cardPath = "/c/" + key + "/" + String(card.id) + "-" + dashedName;
+    const boardPath = "/b/" + board_key + "/" + board_name;
 
     const cardClick = () => {
       setCardPage(true);
       window.history.pushState({data: '바뀐 주소와 함께 저장할 데이터 객체가 이 첫 번째 파라미터. 바뀔 페이지의 정보들을 담아두고 클라이언트에서 정보를 활용해 새로운 페이지를 렌더링하면 된다. 정보는 history.state로 접근하면 된다.'}, '바꿀 제목', cardPath); // 바꿀 주소 앞에 점 찍으면 상대 주소 됨. 우리는 해당 사항 없음
+    }
+
+    const exitModal = () => {
+      setCardPage(false);
+      window.history.pushState({data: '바뀐 주소와 함께 저장할 데이터 객체가 이 첫 번째 파라미터. 바뀔 페이지의 정보들을 담아두고 클라이언트에서 정보를 활용해 새로운 페이지를 렌더링하면 된다. 정보는 history.state로 접근하면 된다.'}, '바꿀 제목', boardPath); // 바꿀 주소 앞에 점 찍으면 상대 주소 됨. 우리는 해당 사항 없음
     }
 
     /*TODO history 없이 띡 /c/로 시작하는 url이 입력됐다면 어떻게 할 지 결정할 것!*/
@@ -22,10 +28,10 @@ function Card({ card, index }) {
       <>
         <div className="board-card" 
           onClick={cardClick}
-          style={index === 0 ? { marginTop: 0 } : { marginTop: 10 }}>
+          style={{ marginTop: (index === 0 ? 0 : 10)}}>
           <p style={{wordBreak: "break-all", color: 'black'}}>{card.name}</p>
         </div>
-        {cardPage? <CardModal card_key={key} card_id={card.id} card_name={card.name}/> : <></>}
+        {cardPage? <CardModal card_key={key} card_id={card.id} card_name={card.name} exit={exitModal}/> : <></>}
       </>
     );
 }
--- src/Components/CardModal/CardModal.js
+++ src/Components/CardModal/CardModal.js
@@ -1,48 +1,31 @@
 import React from 'react';
 import './CardModal.css';
 
-function CardModal({card_key, card_id, card_name}) {
+function CardModal({card_key, card_id, card_name, exit}) {
+
+  const exitIfNotModal = (e) => {
+    if (e.target.id.includes("card-modal-wrapper") || e.target.className === "blank-for-card-modal" || e.target.id === "card-modal-x") {
+      exit();
+    }
+  }
 
   return(
-    <div id="card-modal-wrapper">
-      <div id="card-modal">
+    <div id="card-modal-wrapper" onClick={exitIfNotModal}>
+      <div id="card-modal-wrapper-2">
+        <div className="blank-for-card-modal"/>
 
-        <h3>Card named {card_name}</h3>
-        <p>in list /*TODO listname*/</p>
-       
-        <div id="card-modal-left">
-          <h4>Description</h4>
-          <p>Add a more detailed description...</p>
-          <h4>Activity</h4>
-          <button>Hide Details</button>
-          <p>TODO PIC</p><input placeholder="Write a comment..."/>
-          <p>TODO 댓글목록 ul li ...</p>
-        </div>
+        <div id="card-modal" onClick={console.log("modal!")}>
 
-        <div id="card-modal-right">
-          <p>SUGGESTED</p>
-            <button>톱니TODO</button>
-          <p>ADD TO CARD</p>
-            <button>Members</button>
-            <button>Labels</button>
-            <button>Checklist</button>
-            <button>Due Date</button>
-            <button>Attachment</button>
-            <button>Cover</button>
-          <p>POWER-UPS</p>
-            <button>+ Add Power-Ups</button>
-            <p>Get unlimited Power-Ups, plus much more.</p>
-            <button>Upgrade Team</button>
-          <p>BUTLER <span>NEW</span><span>INFO TODO</span></p>
-            <button>+ Add Card Button</button>
-          <p>ACTIONS</p>
-            <button>Move</button>
-            <button>Copy</button>
-            <button>Make Template</button>
-            <button>Watch</button>
-            <button>Archive</button>
-            <button>Share</button>
+          <div id="card-modal-top">
+            <h3>Card named {card_name} <br/><br/><button id="card-modal-x">X</button></h3>
+            <p>in list /*TODO listname*/</p>
+          </div>
+
+          <div id="card-modal-bottom"/>        
+
         </div>
+        
+        <div className="blank-for-card-modal"/>
 
       </div>
     </div>
@@ -50,3 +33,41 @@ function CardModal({card_key, card_id, card_name}) {
 }
 
 export default CardModal;
+
+/*
+          <div id="card-modal-bottom">
+              <div id="card-modal-left" style={{columnWidth: 400}}>
+                <h4>Description</h4>
+                <p>Add a more detailed description...</p>
+                <h4>Activity</h4>
+                <button>Hide Details</button>
+                <p>TODO PIC</p><input placeholder="Write a comment..."/>
+                <p>TODO 댓글목록 ul li ...</p>
+              </div>
+
+              <div id="card-modal-right" style={{columnWidth: 200}}>
+                <p>SUGGESTED</p>
+                  <button>톱니TODO</button>
+                <p>ADD TO CARD</p>
+                  <button>Members</button>
+                  <button>Labels</button>
+                  <button>Checklist</button>
+                  <button>Due Date</button>
+                  <button>Attachment</button>
+                  <button>Cover</button>
+                <p>POWER-UPS</p>
+                  <button>+ Add Power-Ups</button>
+                  <p>Get unlimited Power-Ups, plus much more.</p>
+                  <button>Upgrade Team</button>
+                <p>BUTLER <span>NEW</span><span>INFO TODO</span></p>
+                  <button>+ Add Card Button</button>
+                <p>ACTIONS</p>
+                  <button>Move</button>
+                  <button>Copy</button>
+                  <button>Make Template</button>
+                  <button>Watch</button>
+                  <button>Archive</button>
+                  <button>Share</button>
+              </div>
+            </div>
+            */
