8032db6dedfdaba91576987c5bdc1f8a5ebef113
yuzzznk <69066745+yuzzznk@users.noreply.github.com>
2020-12-25 16:12:57 +0900

    .

--- src/Pages/Card/Card.js
+++ src/Pages/Card/Card.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Card } from '../../Components';
+import BoardPage from '../Board/Board.js';
+
 
 function CardPage(props) {
   const key = props.match.params.card_key;
   const id_and_name = props.match.params.card_name;
   const id = parseInt(id_and_name.split("-")[0]);
   const name = id_and_name.replace(id + "-", "").replaceAll("-", " ");
-  console.log("key and name is:");
-  console.log(key);
-  console.log(id);
-  console.log(name);
+
   return(
-    <Card/>
+    <><BoardPage/>
+      <Route
+        path={routes.CardPage.path}
+        component={routes.CardPage.component}
+      />
+    <Card/></>
   );
 }
 
