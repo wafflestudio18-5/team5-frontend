f4421c6343c06c968010efa193b5e67a6c84e7fa
yuzzznk <69066745+yuzzznk@users.noreply.github.com>
2020-12-25 12:44:23 +0900

    Update Card.js

--- src/Components/Board/Card.js
+++ src/Components/Board/Card.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Board.css";
 
 function Card({ card, index }) {
-  console.log(card);
     return (
       <div className="board-card" style={index === 0 ? { marginTop: 0 } : { marginTop: 10 }}>
         <p style={{wordBreak: "break-all"}}>{card.name}</p>
