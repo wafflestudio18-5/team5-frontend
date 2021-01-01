5664bbf757f4209b7857f2b031777e2ba0b166ac
yuzzznk <69066745+yuzzznk@users.noreply.github.com>
2021-01-01 21:19:33 +0900

    Update Activity.js

--- src/Components/CardModal/Activity.js
+++ src/Components/CardModal/Activity.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 function Activity({data, board_id, card_id}) {
+
+    const inputRef = useRef();
 
     const [changedComment, setChangedComment] = useState(data.content);
     const [button, setButton] = useState({editMode: false, green: true})
@@ -25,10 +27,14 @@ function Activity({data, board_id, card_id}) {
         // DELETE /api/v1/activity/
         console.log("DELETE");
     }
+
+    const editCommentClick = () => {
+        setButton({...button, editMode: true});
+        inputRef.current.focus();
+    }
     const putCommentClick = () => {
         setButton({...button, editMode: false})
         putComment(changedComment, data.id)
-        setChangedComment("");
     }
 
     const deleteCommentClick = () => {
@@ -39,12 +45,12 @@ function Activity({data, board_id, card_id}) {
             <img src="" alt={String(data.id)}/> {/*TODO 프사설정*/}
             <nobr/>
             {data.is_comment ? <>
-            {button.editMode
-                ? <>
+                <div style={button.editMode? null : {display: 'none'}}>
                     <input
+                    ref={inputRef}
                     value={changedComment}
                     onChange={commentChange}
-                    onBlur={(e) => (e.target.value === "" || e.target.value === data.content) ? setButton({editMode: false, green: false}) : null}
+                    onBlur={(e) => (e.target.value === "" || e.target.value === data.content) ? setButton({...button, editMode: false}) : null}
                     id="card-comment"
                     placeholder="Edit your comment..."/>
 
@@ -52,11 +58,11 @@ function Activity({data, board_id, card_id}) {
                     onClick={putCommentClick}
                     style={{backgroundColor: button.green? 'green' : 'lightgray', color: button.green? 'white' : 'gray'}}>
                     Save
-                    </button></>
-                : <><p>{data.content}</p>
-                <button className="ActivityCommentModify" onClick={() => setButton({...button, editMode: true})}>edit</button>
+                    </button></div>
+                <div style={button.editMode? {display: 'none'} :null}><p>{data.content}</p>
+                <button className="ActivityCommentModify" onClick={editCommentClick}>edit</button>
                 <button className="ActivityCommentModify" onClick={deleteCommentClick}>delete</button>
-            </>}
+            </div>
             </> : null}
             {/* TODO 댓글이 아닐 경우 다른 방식으로... */}
         </div>
