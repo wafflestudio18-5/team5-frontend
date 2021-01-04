a59909915d256db6af6fb3e4caaa6ee69fc29a33
yuzzznk <69066745+yuzzznk@users.noreply.github.com>
2021-01-04 22:19:23 +0900

    .

--- src/Components/CardModal/CardModal.js
+++ src/Components/CardModal/CardModal.js
@@ -18,14 +18,42 @@ function CardModal({setCardName, card_key, card, exit, list_name, board_id, post
     (e.target.value === "") ? setButton({...button, green: false}) : setButton({...button, green: true})
   }
 
+  //댓글 달고 저장하기 TODO 안 뜸 아놔
   const saveComment = () => {
-    postActivity(card.id, comment);
+    //postActivity(card.id, comment);
+
+    axios.post("/api/v1/activity/", { card_id: String(card.id), content: comment })
+    .then(function(response) {
+        console.log("댓글 달기 성공");
+        console.log(response);
+    })
+    .catch(function (error) {
+    if (error.response) {
+      console.log("// 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.");
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    }
+    else if (error.request) {
+      console.log("// 요청이 이루어 졌으나 응답을 받지 못했습니다.");
+      // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
+      // Node.js의 http.ClientRequest 인스턴스입니다.
+      console.log(error.request);
+    }
+    else {
+      console.log("// 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.");
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });
+
     setComment("");
     setButton({display: false, green: false});
   }
 
+  //해당 카드 지우기
   const deleteCardClick = (card_id) => {
-    //deleteCard();
+
     axios.delete("/api/v1/card/", { id: String(card_id) })
     .then(function(response) {
         console.log("카드 삭제 성공");
@@ -49,11 +77,15 @@ function CardModal({setCardName, card_key, card, exit, list_name, board_id, post
     }
     console.log(error.config);
   });
+
     exit();
   }
 
   //카드의 제목 바꾸기
   const [nameState, setNameState] = useState({name: card.name, edit: false});
+  const cardNameChange = (e) => {
+    setNameState({...nameState, name: e.target.value});
+  }
   const changeName = (card_id, name) => {
     axios.put("/api/v1/card/", { id: card_id, name: name })
     .then(function(response) {
@@ -80,7 +112,7 @@ function CardModal({setCardName, card_key, card, exit, list_name, board_id, post
   });
 
   setNameState({...nameState, edit: false});
-  setCardName(nameState.name);
+  setCardName(nameState.name); // 리스트 화면에 보이는 카드 이름 변경
   }
 
   //Description 추가 및 변경하기
@@ -93,6 +125,7 @@ function CardModal({setCardName, card_key, card, exit, list_name, board_id, post
   const addDuedate = () => {
     alert("[ERROR] NO ACTIVITY PREPARED EXCEPT COMMENTS\n(Failed to add Due Date)");
   }
+
   console.log(card);
 
   return(
@@ -100,13 +133,13 @@ function CardModal({setCardName, card_key, card, exit, list_name, board_id, post
       <div id="card-modal-wrapper-2">
         <div className="blank-for-card-modal"/>
 
-        <div id="card-modal" onClick={console.log("modal!")}>
+        <div id="card-modal">
 
           <div id="card-modal-top">
             {!(nameState.edit)? <p style={{fontWeight: 700, fontSize: 20}}
             onClick={() => setNameState({...nameState, edit: true})}>
               <br/><br/><br/><br/><br/><br/>{nameState.name}
-            </p> : <><br/><br/><br/><br/><br/><br/><input style={{fontWeight: 700, fontSize: 20}} value={nameState.name} onChange={(e) => setNameState({...nameState, name: e.target.value})} onBlur={() => nameState.edit? changeName(card.id, nameState.name) : null}/></>}
+            </p> : <><br/><br/><br/><br/><br/><br/><input style={{fontWeight: 700, fontSize: 20}} value={nameState.name} onChange={cardNameChange} onBlur={() => nameState.edit? changeName(card.id, nameState.name) : null}/></>}
             <button id="card-modal-x"/>
             <p id="card-modal-listname">in list <span style={{textDecoration: 'underline'}}>{list_name}</span></p>
           </div>
@@ -138,12 +171,11 @@ function CardModal({setCardName, card_key, card, exit, list_name, board_id, post
                   Save
                 </button>
                 <p>TODO 댓글목록 ul li ...</p>
-                {/*card.activities.map((data, index) => (
-                  <><Activity data={data} key={index} board_id={board_id} card_id={card.id}
-                    putActivity={putActivity} deleteActivity={deleteActivity}
+                {/*카드 액티비티가 undefined라고 뜸 콘솔띄워도 안 뜸 엉엉엉 (card.activities !== [])? card.activities.map((data, index) => (
+                  <><Activity data={data} key={index}
                   />
                   <br/></>
-                 ))*/}
+                 )) : null*/}
                 <button onClick={() => deleteCardClick(card.id)}>Delete Card</button>
               </div>
 
