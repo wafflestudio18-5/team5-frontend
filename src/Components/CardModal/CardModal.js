bd99ef1558d36d20cfeb2cd67bb07a78e7f77912
woohm402 <woohm404@gmail.com>
2021-01-09 00:19:06 +0900

    bug fix

--- src/Components/CardModal/CardModal.js
+++ src/Components/CardModal/CardModal.js
@@ -1,23 +1,38 @@
 import React, { useState, useEffect } from "react";
 import "./CardModal.css";
 import Activity from "./Activity.js";
-import apis from '../../Library/Apis';
+import apis from "../../Library/Apis";
 
-function CardModal({ cardName, setCardName, card_key, card_id, exit, list_name, board_id, putCard, deleteCard, postActivity, putActivity, deleteActivity }) {
+function CardModal({
+  cardName,
+  setCardName,
+  card_key,
+  card_id,
+  exit,
+  list_name,
+  board_id,
+  putCard,
+  deleteCard,
+  postActivity,
+  putActivity,
+  deleteActivity,
+}) {
   const [card, setCard] = useState(undefined);
   const [nameState, setNameState] = useState({ name: cardName, edit: false });
   const [refresh, setRefresh] = useState(false);
 
   function getCard() {
-    apis.card.getByKey({key: card_key})
+    apis.card
+      .getByKey({ key: card_key })
       .then(function (response) {
         setCard(response.data);
         setNameState({ ...nameState, name: response.data.name });
-        setDescription({...description, content: response.data.description.content})
+        setDescription({ ...description, content: response.data.description });
       })
       .catch(function (error) {
         if (error.response) {
-          console.log("요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다."
+          console.log(
+            "요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다."
           );
           console.log(error.response.data);
           console.log(error.response.status);
@@ -28,7 +43,8 @@ function CardModal({ cardName, setCardName, card_key, card_id, exit, list_name, 
           // Node.js의 http.ClientRequest 인스턴스입니다.
           console.log(error.request);
         } else {
-          console.log("오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다."
+          console.log(
+            "오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다."
           );
           console.log("Error", error.message);
         }
@@ -76,12 +92,13 @@ function CardModal({ cardName, setCardName, card_key, card_id, exit, list_name, 
     setNameState({ ...nameState, name: e.target.value });
   };
   const changeName = (card_id, name) => {
-    apis.card.put( { id: card_id, name })
-      .then(function (response) {
-      })
+    apis.card
+      .put({ id: card_id, name })
+      .then(function (response) {})
       .catch(function (error) {
         if (error.response) {
-          console.log("요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다."
+          console.log(
+            "요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다."
           );
           console.log(error.response.data);
           console.log(error.response.status);
@@ -92,7 +109,8 @@ function CardModal({ cardName, setCardName, card_key, card_id, exit, list_name, 
           // Node.js의 http.ClientRequest 인스턴스입니다.
           console.log(error.request);
         } else {
-          console.log("오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다."
+          console.log(
+            "오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다."
           );
           console.log("Error", error.message);
         }
@@ -111,7 +129,7 @@ function CardModal({ cardName, setCardName, card_key, card_id, exit, list_name, 
   });
   useEffect(() => {
     // 서버에 description 변경
-    putCard({cId: card_id, description: description})
+    putCard({ cId: card_id, description: description.content });
   }, [description.edit && false]);
 
   // 멤버 추가하기
@@ -128,29 +146,55 @@ function CardModal({ cardName, setCardName, card_key, card_id, exit, list_name, 
   };
 
   return (
-    <div id="card-modal-wrapper" onClick={exitIfNotModal} style={{cursor: 'default'}}>
-      <div id="card-modal-wrapper-2" style={{display: 'flex', flexDirection: 'column'}}>
+    <div
+      id="card-modal-wrapper"
+      draggable="false"
+      onClick={exitIfNotModal}
+      style={{ cursor: "default" }}
+    >
+      <div
+        id="card-modal-wrapper-2"
+        style={{ display: "flex", flexDirection: "column" }}
+      >
         <div className="blank-for-card-modal" />
 
         <div id="card-modal">
           <div id="card-modal-top">
-          <div style={{display: 'float'}}>
-            {!nameState.edit ? (
-              <p style={{ fontWeight: 700, fontSize: 20, float:'left' }}
-                onClick={() => setNameState({ ...nameState, edit: true })}>
-                {nameState.name}
-              </p>
-            ) : (
-              <>
+            <div style={{ display: "float" }}>
+              {nameState.edit ? (
+                <>
                   <input
-                  style={{ fontWeight: 700, fontSize: 20, float: 'left', marginBottom: 3 }}
-                  value={nameState.name}
-                  onChange={cardNameChange}
-                  onBlur={() => nameState.edit ? changeName(card_id, nameState.name) : null}
-                />
-              </>
-            )}
-            <button style={{ float: 'right', display: 'inline-block' }} className="card-modal-x" id="exit">×</button></div>
+                    style={{
+                      fontWeight: 700,
+                      fontSize: 20,
+                      float: "left",
+                      marginBottom: 3,
+                    }}
+                    value={nameState.name}
+                    onChange={cardNameChange}
+                    onBlur={() =>
+                      nameState.edit
+                        ? changeName(card_id, nameState.name)
+                        : null
+                    }
+                  />
+                </>
+              ) : (
+                <p
+                  style={{ fontWeight: 700, fontSize: 20, float: "left" }}
+                  onClick={() => setNameState({ ...nameState, edit: true })}
+                >
+                  {nameState.name}
+                </p>
+              )}
+              <button
+                style={{ float: "right", display: "inline-block" }}
+                className="card-modal-x"
+                id="exit"
+              >
+                ×
+              </button>
+            </div>
             <p id="card-modal-listname">
               in list{" "}
               <span style={{ textDecoration: "underline" }}>{list_name}</span>
@@ -161,98 +205,159 @@ function CardModal({ cardName, setCardName, card_key, card_id, exit, list_name, 
             <div id="card-modal-left" style={{ columnWidth: 400 }}>
               <p className="title">Description</p>
               {description.edit ? (
-                  <textarea
-                    style={{marginLeft: 5, borderRadius: 5, outline: 'none', height: 40, width: 485, border: '1px solid lightgray', marginRight: 5}}
-                    onKeyPress={(e) => e.key === "Enter"? 
-                    (e.target.value === ""? setDescription({...description, edit: false}) : setDescription({...description, exist: true, edit: false})): null}
-                    value={description.content}
-                    onChange={(e) =>
-                      setDescription({
-                        ...description,
-                        content: e.target.value,
-                      })
-                    }
-                  />
-                ) : ((description.content !== "" && description.content !== undefined) ? (
-                  <p onClick={() => setDescription({ ...description, edit: true })}>
-                    {description.content}
-                  </p>
-                )
-               : (
+                <textarea
+                  style={{
+                    marginLeft: 5,
+                    borderRadius: 5,
+                    outline: "none",
+                    height: 40,
+                    width: 485,
+                    border: "1px solid lightgray",
+                    marginRight: 5,
+                  }}
+                  onKeyPress={(e) => {
+                    e.key === "Enter"
+                      ? !e.target.value
+                        ? setDescription({ ...description, edit: false })
+                        : setDescription({
+                            ...description,
+                            exist: true,
+                            edit: false,
+                          })
+                      : null;
+                  }}
+                  value={description.content}
+                  onChange={(e) =>
+                    setDescription({
+                      ...description,
+                      content: e.target.value,
+                    })
+                  }
+                />
+              ) : description.content ? (
+                <p
+                  onClick={() => setDescription({ ...description, edit: true })}
+                >
+                  {description.content}
+                </p>
+              ) : (
                 <button
-                  style={{width: 500, textAlign: 'left', height: 50, paddingLeft: 10, marginLeft: 5, paddingTop: 0}}
+                  style={{
+                    width: 500,
+                    textAlign: "left",
+                    height: 50,
+                    paddingLeft: 10,
+                    marginLeft: 5,
+                    paddingTop: 0,
+                  }}
                   onClick={() => setDescription({ ...description, edit: true })}
                   id="card-modal-add-descrip"
                 >
                   Add a more detailed description...
                 </button>
-              ))}
+              )}
 
-              <div style={{display: 'float'}}>
-                <p className="title" style={{float: 'left'}}>
+              <div style={{ display: "float" }}>
+                <p className="title" style={{ float: "left" }}>
                   Activity
                 </p>
-                <button id="card-modal-detail" onClick={(e) => setDetail(!detail)} style={{float: 'right', display: 'inline-block', width: 100}}>{detail? "Hide Details" : "Show Details"}</button>
-              </div>
-
-              <div id="card-modal-activities" style={{height: button.display? 242 : 280, maxHeight: button.display? 242 : 280, overflowX: 'auto', marginTop: 20}}>              
-              <div style={{display: 'flex', flexDirection: 'row'}}>
-                <img style={{
-                  height: 35, width: 35, borderRadius: '50%', marginBottom: 15, marginRight: 10, position: 'relative', top: 4, left: 2}} 
-                  src="https://assets.leetcode.com/users/bundhoo/avatar_1527798889.png" alt={String(cardName)}/> {/*TODO 프사설정*/}
-                <div style={{
-                  backgroundColor: 'white', 
-                  padding: 3, 
-                  paddingBottom: 0,
-                  border: '1.5px lightgray solid',
-                  borderRadius: 3,
-                  marginBottom: 5
-                  }}>
-                  <input
-                  style={{fontSize: 15}}
-                    value={comment}
-                    onChange={changeComment}
-                    onFocus={() => setButton({ ...button, display: true })}
-                    onBlur={(e) =>
-                      e.target.value === ""
-                        ? setButton({ display: false, green: false })
-                        : null
-                    }
-                    id="card-comment"
-                    placeholder="Write a comment..."
-                  />
-
                 <button
-                  onClick={saveComment}
+                  id="card-modal-detail"
+                  onClick={(e) => setDetail(!detail)}
                   style={{
-                    display: button.display ? null : "none",
-                    backgroundColor: button.green ? "#5AAC44" : "lightgray",
-                    color: button.green ? "white" : "gray",
-                    marginTop: button.display? 7 : 0,
-                    marginLeft: button.display? 7 : 0,
-                    marginBottom: 10,
-                    width: 50,
-                    height: 30
+                    float: "right",
+                    display: "inline-block",
+                    width: 100,
                   }}
                 >
-                  Save
-                </button></div>
+                  {detail ? "Hide Details" : "Show Details"}
+                </button>
               </div>
 
-                {card !== undefined
-                  ? card.activities.slice().reverse().map((data, index) => (
-                    <Activity
-                      data={data}
-                      refresh={refresh}
-                      setRefresh={setRefresh}
-                      key={index}
-                      postActivity={postActivity}
-                      putActivity={putActivity}
-                      deleteActivity={deleteActivity}
-                      detail={detail}
+              <div
+                id="card-modal-activities"
+                style={{
+                  height: button.display ? 242 : 280,
+                  maxHeight: button.display ? 242 : 280,
+                  overflowX: "auto",
+                  marginTop: 20,
+                }}
+              >
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <img
+                    style={{
+                      height: 35,
+                      width: 35,
+                      borderRadius: "50%",
+                      marginBottom: 15,
+                      marginRight: 10,
+                      position: "relative",
+                      top: 4,
+                      left: 2,
+                    }}
+                    src="https://assets.leetcode.com/users/bundhoo/avatar_1527798889.png"
+                    alt={String(cardName)}
+                  />{" "}
+                  {/*TODO 프사설정*/}
+                  <div
+                    style={{
+                      backgroundColor: "white",
+                      padding: 3,
+                      paddingBottom: 0,
+                      border: "1.5px lightgray solid",
+                      borderRadius: 3,
+                      marginBottom: 5,
+                    }}
+                  >
+                    <input
+                      style={{ fontSize: 15 }}
+                      value={comment}
+                      onChange={changeComment}
+                      onFocus={() => setButton({ ...button, display: true })}
+                      onBlur={(e) =>
+                        e.target.value === ""
+                          ? setButton({ display: false, green: false })
+                          : null
+                      }
+                      id="card-comment"
+                      placeholder="Write a comment..."
                     />
-                  ))
-                : null}
+
+                    <button
+                      onClick={saveComment}
+                      style={{
+                        display: button.display ? null : "none",
+                        backgroundColor: button.green ? "#5AAC44" : "lightgray",
+                        color: button.green ? "white" : "gray",
+                        marginTop: button.display ? 7 : 0,
+                        marginLeft: button.display ? 7 : 0,
+                        marginBottom: 10,
+                        width: 50,
+                        height: 30,
+                      }}
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+
+                {card !== undefined
+                  ? card.activities
+                      .slice()
+                      .reverse()
+                      .map((data, index) => (
+                        <Activity
+                          data={data}
+                          refresh={refresh}
+                          setRefresh={setRefresh}
+                          key={index}
+                          postActivity={postActivity}
+                          putActivity={putActivity}
+                          deleteActivity={deleteActivity}
+                          detail={detail}
+                        />
+                      ))
+                  : null}
               </div>
             </div>
 
@@ -272,13 +377,49 @@ function CardModal({ cardName, setCardName, card_key, card_id, exit, list_name, 
               </p>
               <button>+ Add Power-Ups</button>
               <p>Get unlimited Power-Ups, plus much more.</p>
-              <button style={{background: '#EDDBF4', paddingTop: 3}}><img style={{position: 'relative', top: 2, marginRight: 3}} src='https://api.iconify.design/octicon:heart-24.svg?height=14'/>Upgrade Team</button>
+              <button style={{ background: "#EDDBF4", paddingTop: 3 }}>
+                <img
+                  style={{ position: "relative", top: 2, marginRight: 3 }}
+                  src="https://api.iconify.design/octicon:heart-24.svg?height=14"
+                />
+                Upgrade Team
+              </button>
               <p>
                 <br />
-                BUTLER <span style={{color: 'green', background: '#D6ECD2', marginLeft: 3, padding: 3, paddingLeft: 7, paddingRight: 7, borderRadius: 10}}>NEW</span> <span style={{position: 'relative', top: 2, left: 30, content: "url('https://api.iconify.design/octicon:info-24.svg?height=15')", verticalAlign: '-0.125em'}}/>
+                BUTLER{" "}
+                <span
+                  style={{
+                    color: "green",
+                    background: "#D6ECD2",
+                    marginLeft: 3,
+                    padding: 3,
+                    paddingLeft: 7,
+                    paddingRight: 7,
+                    borderRadius: 10,
+                  }}
+                >
+                  NEW
+                </span>{" "}
+                <span
+                  style={{
+                    position: "relative",
+                    top: 2,
+                    left: 30,
+                    content:
+                      "url('https://api.iconify.design/octicon:info-24.svg?height=15')",
+                    verticalAlign: "-0.125em",
+                  }}
+                />
               </p>
               <button>+ Add Card Button</button>
-              <button style={{marginTop: 15, fontWeight: 600, border: '1px lightgray solid'}} onClick={() => deleteCardClick(card_id)}>
+              <button
+                style={{
+                  marginTop: 15,
+                  fontWeight: 600,
+                  border: "1px lightgray solid",
+                }}
+                onClick={() => deleteCardClick(card_id)}
+              >
                 Delete Card
               </button>
             </div>
